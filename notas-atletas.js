c0483dfe20a6ff8cb2c4deb26351e5084e8c9044
Luiz Aurelio <aurelioluizaurelio@gmail.com>
2025-12-27 17:38:38 -0300

    projeto finalizado

---
+++ notas-atletas.js
@@ -0,0 +1,36 @@
+let atletas = [
+    {
+        nome: "Cesar Abascal",
+        notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+        nome: "Fernando Puntel",
+        notas: [8, 10, 10, 7, 9.33]
+    },
+    {
+        nome: "Daiane Jelinsky",
+        notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+        nome: "Bruno Castro",
+        notas: [10, 10, 10, 9, 9.5]
+    }
+];
+
+function mediasAtletas(atletas) {
+    atletas.forEach(atleta => {
+        let notasOrdenadas = atleta.notas.slice().sort((a, b) => a - b);
+        let notasValidas = notasOrdenadas.slice(1, -1);
+        let soma = notasValidas.reduce((a, v) => a + v, 0);
+        let media = soma / notasValidas.length;
+
+        console.log(`Atleta: ${atleta.nome}`);
+        console.log(`Notas Obtidas: ${notasOrdenadas.join(',')}`);
+        console.log(`Média Válida: ${media}`);
+        console.log('');
+    });
+}
+
+mediasAtletas(atletas);
+
+
